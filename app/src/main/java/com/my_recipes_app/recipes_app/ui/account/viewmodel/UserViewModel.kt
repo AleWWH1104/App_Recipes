094c31b8d5388f5df2b0fa7ee651aef3b496e744
AleWWH1104 <irisale.ayala@gmail.com>
2025-02-13 14:52:11 -0600

    fix(viewmodel): Se arreglo funciones como checksession y deleteAccount

--- app/src/main/java/com/my_recipes_app/recipes_app/ui/account/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/my_recipes_app/recipes_app/ui/account/viewmodel/UserViewModel.kt
@@ -1,23 +1,21 @@
 package com.my_recipes_app.recipes_app.ui.account.viewmodel
 
+import android.app.Application
 import android.content.Context
-import android.content.SharedPreferences
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.my_recipes_app.recipes_app.database.recipes.UsersWithRecipes
 import com.my_recipes_app.recipes_app.database.users.UserEntity
 import com.my_recipes_app.recipes_app.ui.account.repository.UsersRepository
 import kotlinx.coroutines.launch
 
-class UserViewModel(private val repository: UsersRepository, private val context: Context): ViewModel() {
+class UserViewModel(private val repository: UsersRepository, application: Application): AndroidViewModel(application) {
 
-    private val _isLoading = MutableLiveData<Boolean>()
-    val isLoading: LiveData<Boolean> = _isLoading
-
-    private val _errorMessage = MutableLiveData<String>()
-    val errorMessage: LiveData<String> get() = _errorMessage
+    private val _errorMessage = MutableLiveData<String?>()
+    val errorMessage: LiveData<String?> = _errorMessage
 
     private val _user = MutableLiveData<UserEntity?>()
     val user : LiveData<UserEntity?> = _user
@@ -25,8 +23,7 @@ class UserViewModel(private val repository: UsersRepository, private val context
     private val _userWithRecipes = MutableLiveData<UsersWithRecipes>()
     val userWithRecipes: LiveData<UsersWithRecipes> get() = _userWithRecipes
 
-    private val sharedPreferences: SharedPreferences =
-        context.getSharedPreferences("user_session", Context.MODE_PRIVATE)
+    private val sharedPreferences = application.getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
 
     fun signIn(email: String, password: String){
         if (email.isBlank() || password.isBlank()){
@@ -38,6 +35,7 @@ class UserViewModel(private val repository: UsersRepository, private val context
             if (userExist != null){
                 _user.value = userExist
                 saveSession(userExist)
+                _errorMessage.value = null
             } else{
                 _errorMessage.value = "Email o contraseña incorrectos"
             }
@@ -51,6 +49,7 @@ class UserViewModel(private val repository: UsersRepository, private val context
                 repository.insertUserInEntity(newUser)
                 _user.value = newUser
                 saveSession(newUser)
+                _errorMessage.value = null
             }
         }
     }
@@ -62,9 +61,15 @@ class UserViewModel(private val repository: UsersRepository, private val context
 
     fun deleteAccount(){
         viewModelScope.launch {
-            _user.value?.let {
-                repository.deleteUserInEntity(it)
-                _user.value = null
+            _user.value?.let { user ->
+                repository.deleteUserInEntity(user)
+                val userCheck = repository.getUserFromEntity(user.email, user.password)
+                if (userCheck == null) {
+                    clearSession()
+                    _user.postValue(null)
+                } else {
+                    Log.e("DeleteAccount", "El usuario aún existe en la base de datos")
+                }
             }
         }
     }
@@ -93,6 +98,13 @@ class UserViewModel(private val repository: UsersRepository, private val context
         sharedPreferences.edit().clear().apply()
     }
 
+    fun checkSession(){
+        val currentUser = getSession()  // Recupera la sesión guardada
+        if (currentUser != null) {
+            _user.value = currentUser  // Si la sesión está activa, actualiza el usuario en el ViewModel
+        }
+    }
+
     private fun validateInputs(username: String, email: String, password: String): Boolean{
         if (username.isBlank() || email.isBlank() || password.isBlank()) {
             _errorMessage.value = "Todos los campos son obligatorios"
@@ -101,5 +113,10 @@ class UserViewModel(private val repository: UsersRepository, private val context
         //Aqui se puede implementar restricciones para las contrasenas o para el email
         return true
     }
+
+    fun clearError() {
+        _errorMessage.value = null
+    }
+
 
 }
